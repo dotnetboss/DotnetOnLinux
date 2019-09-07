c114c2f8910ecf76c9e2b6c834d07c7b58b381d5
Klein Houzin <klein.houzin@outlook.com>
2019-09-07 19:55:47 +0400

    Initial Commit - Working asp.net application with Sql server on linux

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace app {
+    public class Program {
+        public static void Main (string[] args) {
+            var config = new ConfigurationBuilder ().AddCommandLine (args).Build ();
+
+            CreateWebHostBuilder (args).Build ().Run ();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder (string[] args) {
+
+            var config = new ConfigurationBuilder ().AddCommandLine (args).Build ();
+
+            Console.WriteLine (config);
+
+            return WebHost.CreateDefaultBuilder (args)
+                .UseConfiguration (config)
+                .UseStartup<Startup> ();
+        }
+
+    }
+}
